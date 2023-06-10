60937fc1abb58ef37ad0ff5362d3123b1056f1db
Gagandeep Singh <gagan@Gagandeeps-MacBook-Air.local>
2023-06-10 13:42:29 +0530

    error handling

--- main.py
+++ main.py
@@ -1,9 +1,26 @@
 from flask import Flask,request
-import utils
+import instaloader
+import os
+
+from dotenv import load_dotenv
+load_dotenv()
+
 app = Flask(__name__)
 
+class APIAuthError(Exception):
+  code = 403
+  description = "Authentication Error"
+
+L = instaloader.Instaloader()
+USER = os.getenv("LOGIN")
+PASSWORD = os.getenv("PASSWORD")
+
 @app.route("/download")
-def hello_world():
+def getDownloadLink():
     args = request.args
-    url = args.get("url")
-    return utils.getDownloadLink(url)
+    post_url = args.get("url")
+    try:
+        post = instaloader.Post.from_shortcode(L.context, post_url.split('reel/')[1].split("/")[0])
+        return  post.video_url
+    except:
+        return "An exception occurred"
